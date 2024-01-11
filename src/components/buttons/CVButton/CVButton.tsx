e93093e0f51b696cc00c7841a8ae0a3dd8593893
Michel Lamarlière <michel.lamarliere@icloud.com>
2024-01-11 15:54:09 +0100

    chore: updated cv

--- src/components/buttons/CVButton/CVButton.tsx
+++ src/components/buttons/CVButton/CVButton.tsx
@@ -5,7 +5,7 @@ import classes from "./CVButton.module.scss";
 export function CVButton() {
   return (
     <a
-      href="/cv.pdf"
+      href="/cv-michel-lamarliere.pdf"
       download={"cv-michel-lamarlière-développeur.pdf"}
       className={classes.wrapper}
     >
